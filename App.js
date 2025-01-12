e2f98c4a8c52718891a8bb7baf0c4344c7a0cc46
dcumm002 <dcumm002@gold.ac.uk>
2025-01-12 23:46:17 +0000

    Set up BSL App

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      // This is the launch screen when starting the app
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text>Welcome to Simple Signs!</Text>
+        <Button
+          title = "Sign Up"
+        />
+        <Button
+          title = "Log In"
+        />
+        <Button
+          title = "Continue as Guest"
+        />
+      </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +29,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
